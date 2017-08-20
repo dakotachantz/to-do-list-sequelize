214220b48591a7ecd1544269ca6901e0b122f7ca
Dakota Nichols <dakotachantz@gmail.com>
2017-08-20 01:09:03 -0500

    added breakpoints to make todo list responsive added dummy completed task for demo purposes

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
 module.exports = {
-    todos: [{ task: "finish project", completed: false }, { task: "do reading", completed: false }],
-    markoff: []
+    todos: [{ task: "go skydiving", completed: false }, { task: "discover the meaning of life", completed: false }],
+    markoff: [{ task: "travel to 38 states", completed: true }]
 }
