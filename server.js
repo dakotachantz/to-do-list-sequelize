ed070a0febf9a524ca301d0bbceed98eb407c840
Dakota Nichols <dakotachantz@gmail.com>
2017-08-31 20:00:32 -0500

    added edit delete and complete buttons. added delete all. finished functionality of app and tweaked css.

--- server.js
+++ server.js
@@ -14,49 +14,69 @@ app.set("view engine", "mustache");
 app.use(express.static(path.join(__dirname, "./public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-// models.Todos.findOne()
-//     .then(function (todo) {
-//         console.log(todo.task);
-//     })
-
-
 app.get("/", (req, res) => {
-    let todos;
     models.Todos.findAll().then(function (foundTodos) {
-        // console.log("found stuff: ", foundTodos);
-        todos = foundTodos;
-        // console.log("todos list: ", todos);
-        return res.render("todo", { todos: todos });
+        return res.render("todo", { todos: foundTodos });
     });
 });
 
-app.post("/complete/:task", (req, res) => {
-    let task = req.params.task;
-    let index = data.todos.findIndex(function (item) { return item.task === task });
-    let targetTodo = data.todos[index];
-    targetTodo.completed = !targetTodo.completed;
-    data.markoff.push(targetTodo);
-    data.todos.splice(index, 1);
-    return res.redirect("/");
+app.post("/deleteallcompleted", (req, res) => {
+    models.Todos.destroy({
+        where: {
+            completed: true
+        }
+    }).then(function () {
+        return res.redirect("/");
+    });
+});
+app.post("/edit/:id", (req, res) => {
+    let reqId = req.params.id;
+    console.log(req.body.task);
+    models.Todos.update({
+        task: req.body.task.join("")//did not expect to be an array, refer to post /todo
+    }, {
+            where: {
+                id: reqId
+            }
+        }).then(function () {
+            return res.redirect("/");
+        });
 });
 
-app.post("/todo", (req, res) => {
+app.post("/delete/:id", (req, res) => {
+    let reqId = req.params.id;
+    models.Todos.destroy({
+        where: {
+            id: reqId
+        }
+    }).then(function () {
+        return res.redirect("/");
+    });
+});
 
+app.post("/complete/:id", (req, res) => {
+    let reqId = req.params.id;
+    models.Todos.update({
+        completed: true
+    }, {
+            where: {
+                id: reqId
+            }
+        }).then(function () {
+            return res.redirect("/");
+        });
+});
+
+
+app.post("/todo", (req, res) => {
     const todo = models.Todos.build({
         task: req.body.task,
         completed: false
     });
-
     todo.save()
-        .then(function (newToDo) {
-            res.send(newToDo);
+        .then(function () {
+            return res.redirect("/");
         });
-
-    // let newTodo = req.body;
-    // newTodo.completed = false;
-    // data.todos.push(newTodo);
-    return res.redirect("/");
 });
 
 app.listen(port, () => {
